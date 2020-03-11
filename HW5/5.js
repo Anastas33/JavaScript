f0c53a8a5fcc61461bff0a81d72256a589a1dcb5
Anastasiia Shadrinova <nastya.shadrinova@yandex.ru>
2020-03-11 23:34:13 +0900

    Домашнее задание

--- HW5/5.js
+++ HW5/5.js
@@ -2,33 +2,62 @@
 
 let table = document.querySelector('table');
 
-/* function createBoard() {
-    let board = '';
-    for (let y = 0; y < 9; y++) {
-        board += '<tr>';
-        for (let x = 0; x < 9; x++) {
-            board += `<td data-x=${x} data-y=${y}><td>`;
-        }
-        board +='</tr>';
-    }
-    return `<table>${board}</table>`;
-}
-
-createBoard(); */
-
-/* let ggg = document.createElement('table');
-ggg.innerText = board; */
-
 let board = '';
 for (let y = 0; y < 9; y++) {
     board += '<tr>';
     for (let x = 0; x < 9; x++) {
         board += `<td data-x='${x}' data-y='${y}'></td>`;
-        if (x % 2 == 0 && y % 2 == 1) {
-            td.style.backgroundColor = 'black';
-        }
     }
     board +='</tr>';
 }
 
 table.insertAdjacentHTML('afterbegin', board);
+
+let squares = document.querySelectorAll('td');
+//Заполняю клетки цветами и текстом
+squares.forEach(function(square) {
+    let sqX = square.dataset.x;
+    let sqY = square.dataset.y;
+    if ((sqX % 2 == 0 && sqY % 2 == 1) || (sqX % 2 == 1 && sqY % 2 == 0)) {
+        square.style.backgroundColor = 'black';
+    };
+    if (sqX == 0 || sqY == 0) {
+        square.style.backgroundColor = 'white';
+    };
+
+    if (sqX == 0 && sqY == 1) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 2) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 3) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 4) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 5) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 6) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 7) {
+        square.innerText = sqY;
+    } else if (sqX == 0 && sqY == 8) {
+        square.innerText = sqY;
+    };
+
+    if (sqY == 0 && sqX == 1) {
+        square.innerText = 'A';
+    } else if (sqY == 0 && sqX == 2) {
+        square.innerText = 'B';
+    } else if (sqY == 0 && sqX == 3) {
+        square.innerText = 'C';
+    } else if (sqY == 0 && sqX == 4) {
+        square.innerText = 'D';
+    } else if (sqY == 0 && sqX == 5) {
+        square.innerText = 'E';
+    } else if (sqY == 0 && sqX == 6) {
+        square.innerText = 'F';
+    } else if (sqY == 0 && sqX == 7) {
+        square.innerText = 'G';
+    } else if (sqY == 0 && sqX == 8) {
+        square.innerText = 'H';
+    }
+});
--- HW6/3/3.js
+++ HW6/3/3.js
@@ -1,30 +1,77 @@
 'use strict';
 
 let buttons = document.querySelectorAll('button');
-let cart = document.querySelector('.cart')
+let basket = document.querySelector('.basket')
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function (event) {
-        handleClick(event);
+        let cardNode = event.target.parentNode;
+        let id = cardNode.querySelector('.id').innerText;
+        let name = cardNode.querySelector('.name').innerText;
+        let price = cardNode.querySelector('.price').innerText;
+        cart.addCards({
+            id: id,
+            name: name,
+            price: price
+        });
     });
 });
 
-function handleClick(clickedButtonEvent) {
-    const cardNode = clickedButtonEvent.target.parentNode;
+let cart = {
+    cards: {},
 
-    const card = {
-        wrap: cardNode,
-        id: cardNode.querySelector('.id'),
-        img: cardNode.querySelector('img'),
-        name: cardNode.querySelector('.name'),
-        price: cardNode.querySelector('.price'),
-    };
+    addCards(card) {
+        this.addCardToObject(card);
+        this.addProductToBasket(card);
+        this.makeTotalSum();
+    },
 
-    addToCard(card);
-};
+    addCardToObject(card) {
+        if (this.cards[card.id] == undefined) {
+            this.cards[card.id] = {
+                name: card.name,
+                price: card.price,
+                count: 1
+            }
+        } else {
+            this.cards[card.id].count++;
+        }
+    },
 
-function addToCard(card) {
-    cart.insertAdjacentHTML('afterbegin', card);
-};
+    addProductToBasket(card) {
+        let cardExist = document.querySelector(`.cardCount[data-id='${card.id}']`);
+        if (cardExist) {
+            cardExist.textContent++;
+            return;
+        }
+        let cardDiv = `
+        <div class='product added'>
+            <p class="id">${card.id}</p>
+            <p>Наименование товара: <span class="name"> ${card.name}</span></p>
+            <p>Стоимость товара: <span class="price"> ${card.price}</span> руб.</p>
+            <p>Количество: <span class='cardCount' data-id='${card.id}'> 1</span></p>
+        </div>
+    `;
+    basket.insertAdjacentHTML('beforeend', cardDiv);
+    },
+
+    makeTotalSum() {
+        document.querySelector('.sum').textContent = this.getTotalSum();
+    },
+
+    getTotalSum() {
+        let sum = 0;
+        for (let key in this.cards) {
+            sum += this.cards[key].price * this.cards[key].count;
+        }
+        return sum;
+    }
+}
+
+
+
+    
+    
+
 
 
